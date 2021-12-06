05606444479c5ac99f6543abd4f03419e5bc7fd2
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-06 16:43:45 +0900

    追加機能（todo上限設定）

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -11,11 +11,18 @@ const style = {
 };
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props; //propsから取り出す
+  const { todoText, onChange, onClick, disabled } = props; //propsから取り出す
   return (
     <div style={style}>
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
