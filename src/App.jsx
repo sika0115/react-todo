9362234dda6e4d98f0117a658c54b4d6d61bbd0f
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-06 11:17:52 +0900

    基本機能実装完了

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,65 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  const [incompleteTodos, setInCompleteTodos] = useState([
-    "ああああ",
-    "いいいい"
-  ]);
-  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setInCompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  //追加機能
+  const onClickAdd = () => {
+    if (todoText === "") return; //空文字なら下3行の処理をしない
+    const newTodos = [...incompleteTodos, todoText]; //現在保持しているものに加えて追加したいのでスプレッド構文
+    setInCompleteTodos(newTodos); //行の追加がこれでＯＫ
+    setTodoText(""); //inputの内容をリセット
+  };
+
+  //削除機能
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1); //1つ目の引数に何番目の要素か、2つ目の引数にいくつ削除するかを指定する
+    setInCompleteTodos(newTodos);
+  };
+
+  //完了機能・・・未完了から削除して完了に追加
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1); //1つ目の引数に何番目の要素か、2つ目の引数にいくつ削除するかを指定する
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setInCompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setInCompleteTodos(newIncompleteTodos);
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -31,11 +69,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
